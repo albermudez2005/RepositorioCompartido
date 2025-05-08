c555d80f41ad5363856ae6801710836c60e03f7c
Daniel Cuevas <dcuevas@centronelson.org>
2025-05-08 09:25:15 +0200

    Primer commit

---
+++ src/Pruebas/main.java
@@ -0,0 +1,10 @@
+package Pruebas;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo, esta es la practica 7");
+	}
+
+}
